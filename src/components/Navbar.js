19e8dddf0440c1b040f0e0c440472cae600684e1
Gabo Goses <gabrielmargoses@gmail.com>
2022-01-24 16:53:59 +0100

    feature: reorganize and set UTC time on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,23 @@
+import { useState, useEffect } from 'react';
 import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
+import moment from 'moment';
+
+const Time = () => {
+    const [time, setTime] = useState('');
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(moment().utc().format('HH:mm:ss')), 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return (
+        <div>
+            <p>{`${time} GMT`} </p>
+        </div>
+    );
+};
 
 const Navbar = () => {
     return (
@@ -6,6 +25,7 @@ const Navbar = () => {
             <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
                 <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
                     <Box>Coke Day</Box>
+                    <Time />
                     <Flex alignItems={'center'}></Flex>
                 </Flex>
             </Box>
