42850f9489c3886b3067fe33ef94a18bcd67b872
Gabo Goses <gabrielmargoses@gmail.com>
2022-01-24 16:54:32 +0100

    fix: remove useless button on modal

--- src/components/RoomCard.js
+++ src/components/RoomCard.js
@@ -172,7 +172,6 @@ const RoomCard = (props) => {
                         <Button colorScheme='blue' mr={3} onClick={onClose}>
                             Close
                         </Button>
-                        <Button variant='ghost'>Secondary Action</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
